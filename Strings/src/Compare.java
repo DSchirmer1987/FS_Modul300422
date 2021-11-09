b796d103f8f0c94ac85f579a009763d3d007c137
CC_Dozent <CC_Dozent@DESKTOP-LNFQCA9.localdomain>
2021-11-09 12:11:09 +0100

    String Compare Beispiel

---
+++ Strings/src/Compare.java
@@ -0,0 +1,16 @@
+
+public class Compare {
+
+	public static void main(String[] args) {
+		String a = "Anna";
+		String b = "Bertha";
+		String c = "Christa";
+		
+		System.out.println(a.compareTo(b));
+		System.out.println(b.compareTo(a));
+		
+		System.out.println(a.compareTo(c));
+		System.out.println(c.compareTo(a));
+	}
+
+}
