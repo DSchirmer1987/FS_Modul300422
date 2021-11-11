af6dc57ed88b6e10ccfcfbf92e48c08d280bbb40
CC_Dozent <CC_Dozent@DESKTOP-LNFQCA9.localdomain>
2021-11-11 08:52:59 +0100

    ConfirmDialog - Erklärung

---
+++ JOptionPanes/src/Application.java
@@ -0,0 +1,10 @@
+import confirmDialog.ConfirmDialogDemo;
+
+public class Application {
+
+	public static void main(String[] args) {
+		ConfirmDialogDemo.simpleConfirmDialog();
+
+	}
+
+}
---
+++ JOptionPanes/src/confirmDialog/ConfirmDialogDemo.java
@@ -0,0 +1,50 @@
+package confirmDialog;
+
+import javax.swing.JOptionPane;
+
+public class ConfirmDialogDemo {
+	/*
+	 * JOptionPane.showConfirmDialog
+	 * 
+	 * Ist ein Dialog mit verschiedenen vorgegeben Buttons zum bestätigen.
+	 * 
+	 * Syntax:
+	 * JOptionPane.showConfirmDialog(Component, Object);
+	 * JOptionPane.showConfirmDialog(Component, Object, String, int);
+	 * JOptionPane.showConfirmDialog(Component, Object, String, int, int);
+	 * JOptionPane.showConfirmDialog(Component, Object, String, int, int, Icon);
+	 * 
+	 * Component:
+	 *  - Ist das Fenster in dem der Dialog angezeigt werden soll. Kann auch 'null' sein
+	 *  
+	 * Object: 
+	 *  - Die Message die angezeigt werden soll. Kann alles sein, dass vom Typ Object ist
+	 *  
+	 * String:
+	 *  - Der Titel des OptionPane ConfirmDialog
+	 *  
+	 * int:
+	 *  - Ist der OptionType für JOptionPane
+	 * 		- OptionTypes:
+	 * 			- DEFAULT_OPTION - Standart
+	 * 			- YES_NO_OPTION - Ja/Nein Buttons
+	 * 			- YES_NO_CANCEL_OPTION - Ja/Nein/Abbrechen Buttons
+	 * 			- OK_CANCEL_OPTION - Ok/Abbrechen Buttons
+	 *
+	 * int:
+	 *  - Ist der MessageType (Das Icon / Nachrichtentyp)
+	 *  	- MessageTypes:
+	 *  		- ERROR_MESSAGE
+	 *  		- INFORMATION_MESSAGE
+	 *  		- WARNING_MESSAGE
+	 *  		- QUESTION_MESSAGE
+	 *  		- PLAIN_MESSAGE
+	 *  
+	 * icon:
+	 * 	- Das Icon das angezeigt werden soll. Überschreibt dann den MessageType
+	 */
+	
+	public static void simpleConfirmDialog() {
+		JOptionPane.showConfirmDialog(null, "Mögen Sie Java?");
+	}
+}
