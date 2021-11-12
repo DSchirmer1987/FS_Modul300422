398095c2709489b7148d568ef6d4fe2f9f2ec589
CC_Dozent <d.schirmer1987@gmail.com>
2021-11-12 10:15:08 +0100

    InputDialog Syntax

--- JOptionPanes/src/inputDialog/InputDialogDemo.java
+++ JOptionPanes/src/inputDialog/InputDialogDemo.java
@@ -8,13 +8,23 @@ public class InputDialogDemo {
 	 * Für Eingabe über einen Dialog
 	 * 
 	 * Syntax:
-	 * JOptionPane.showInputDialog(Object)
+	 * JOptionPane.showInputDialog(Object) :
+	 *  - Einfaches Inputfeld mit einer Nachricht (Object = Nachricht)
+	 *  
 	 * JOptionPane.showInputDialog(Object, Object)
+	 *  - Einfaches Inputfeld mit einer Nachricht und einen InitialWert(Object = Nachricht, InitialWert)
+	 *  
 	 * JOptionPane.showInputDialog(Component, Object)
-	 * JOptionPane.showInputDialog(Component, Object, Object)
-	 * JOptionPane.showInputDialog(Component, Object, String, MessageType)
-	 * JOptionPane.showInputDialog(Component, Object, String, MessageType, Icon, Object[], Object)
+	 *  - Einfaches Inputfeld mit einer Nachricht innerhalb eines Fensters(Component = Fenster ,Object = Nachricht)
 	 * 
+	 * JOptionPane.showInputDialog(Component, Object, Object)
+	 *  - Einfaches Inputfeld mit einer Nachricht und einem InitialWert innerhalb eines Fensters(Component = Fenster ,Object = Nachricht, InitialWert)
+	 *  
+	 * JOptionPane.showInputDialog(Component, Object, String, MessageType)
+	 * - Einfaches Inputfeld mit einer Nachricht und einem Titel innerhalb eines Fensters und des MessageTypes (Component = Fenster ,Object = Nachricht)
+	 * 
+	 * JOptionPane.showInputDialog(Component, Object, String, MessageType, Icon, Object[], Object)
+	 * - Inputfeld mit vorgegebenen Werten - Sozusagen ein Dropdown Input mit den Optionen als Object[] und die Startwahl als Object
 	 * 
 	 */
 }
