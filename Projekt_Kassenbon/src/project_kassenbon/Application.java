27b3b4427804adb032cf6e883a8bc686c8b15561
CC_Dozent <d.schirmer1987@gmail.com>
2021-11-22 11:52:00 +0100

    Push wegen leerer Änderung

--- Projekt_Kassenbon/src/project_kassenbon/Application.java
+++ Projekt_Kassenbon/src/project_kassenbon/Application.java
@@ -1,12 +1,16 @@
 package project_kassenbon;
 
+import java.util.ArrayList;
+
 import project_kassenbon.model.Kassenbon;
+import project_kassenbon.model.KassenbonEintrag;
 
 public class Application {
 
 	public static void main(String[] args) {
 		Kassenbon kb = new Kassenbon();
 		System.out.println(kb.getFormattedLaden());
+		
 	}
 
 }
