c61dca4e8c17435a9bd527e85af3c1cad980ce5d
CC_Dozent <d.schirmer1987@gmail.com>
2021-11-22 12:13:56 +0100

    JavaDoc Beispiel

---
+++ Projekt_Kassenbon/Dokumentation/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"project_kassenbon.model","l":"Artikel"},{"p":"project_kassenbon.model","l":"Artikelliste"},{"p":"project_kassenbon.model","l":"Kassenbon"},{"p":"project_kassenbon.model","l":"KassenbonEintrag"}];updateSearchResults();
