874f583df986095428ed7d0523a7e42bc1c1f910
CC_Dozent <d.schirmer1987@gmail.com>
2021-11-26 12:29:47 +0100

    Imports

--- JDBC_Demo/src/schirmer/jdbc_demo/Database.java
+++ JDBC_Demo/src/schirmer/jdbc_demo/Database.java
@@ -6,9 +6,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
