96b5e50ea8c19d5bc9ea028bc34de291290da3c9
CC_Dozent <d.schirmer1987@gmail.com>
2021-11-26 09:14:44 +0100

    Employee Klasse Beispiel

--- JDBC_Demo/src/schirmer/jdbc_demo/Employee.java
+++ JDBC_Demo/src/schirmer/jdbc_demo/Employee.java
@@ -1,5 +1,56 @@
 package schirmer.jdbc_demo;
 
 public class Employee {
+	private int employee_id;
+	private String first_name;
+	private String last_name;
+	private double salary;
+	
+	public Employee(int employee_id, String first_name, String last_name, double salary) {
+		super();
+		this.employee_id = employee_id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.salary = salary;
+	}
 
+	public int getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(int employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employee_id=" + employee_id + ", first_name=" + first_name + ", last_name=" + last_name
+				+ ", salary=" + salary + "]";
+	}
+	
+	
 }
