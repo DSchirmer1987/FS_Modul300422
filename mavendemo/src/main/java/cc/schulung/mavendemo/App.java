44dce5236e30f290b9ff076d7f3218da87cd6023
CC_Dozent <d.schirmer1987@gmail.com>
2021-11-30 10:31:23 +0100

    Mavendemo

---
+++ mavendemo/src/main/java/cc/schulung/mavendemo/App.java
@@ -0,0 +1,27 @@
+package cc.schulung.mavendemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		try {
+			Connection db_conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1/kasse", "root", null);
+			if(db_conn != null) {
+				System.out.println("Connected to the Database");
+			} else {
+				System.out.println("Connection failed");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }
+}
