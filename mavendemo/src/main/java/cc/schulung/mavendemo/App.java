c22b33e4a722403cd62caf6eaf422d7b280c2504
CC_Dozent <d.schirmer1987@gmail.com>
2021-11-30 11:51:56 +0100

    Commit

--- mavendemo/src/main/java/cc/schulung/mavendemo/App.java
+++ mavendemo/src/main/java/cc/schulung/mavendemo/App.java
@@ -1,21 +1,22 @@
 package cc.schulung.mavendemo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
 		try {
 			Connection db_conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1/kasse", "root", null);
-			if(db_conn != null) {
+			if (db_conn != null) {
 				System.out.println("Connected to the Database");
 			} else {
 				System.out.println("Connection failed");
@@ -23,5 +24,5 @@ public class App
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-    }
+	}
 }
